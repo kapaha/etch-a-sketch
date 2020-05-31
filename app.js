86f6da08bce021f6d5d5d7f29a827352ce3c03b8
Kai Paterson - Hall <kaizwebdesign@gmail.com>
2020-05-31 16:44:18 +0100

    added user input by clicking reset button with validation

--- app.js
+++ app.js
@@ -3,29 +3,57 @@
 const gridContainer = document.getElementById('grid-container');
 const gridSize = document.getElementById('grid-size');
 const resetButton = document.getElementById('reset-btn');
+let lastGridSize;
 
 makeRows(16, 16);
-
-const cells = document.querySelectorAll('.grid-item');
-
-cells.forEach((cell) => {
-    cell.addEventListener('mouseover', () => {
-        cell.classList.add('hover-pink');
-    });
-});
+addEventListenerToCells();
 
 resetButton.addEventListener('click', () => {
+    const cells = document.querySelectorAll('.grid-item');
     cells.forEach((cell) => {
         cell.classList.remove('hover-pink');
     });
+    makeRowsFromUserInput();
 });
 
 function makeRows(rows, columns) {
+    lastGridSize = rows;
     gridSize.textContent = `${rows}x${columns} Grid`;
     gridContainer.style.setProperty('--grid-rows', rows);
     gridContainer.style.setProperty('--grid-columns', columns);
     for (let i = 0; i < (rows * columns); i++) {
         let cell = document.createElement('div');
         gridContainer.appendChild(cell).className = 'grid-item';
+    }
+    
+}
+
+function addEventListenerToCells() {
+    const cells = document.querySelectorAll('.grid-item');
+    cells.forEach((cell) => {
+        cell.addEventListener('mouseover', () => {
+            cell.classList.add('hover-pink');
+        });
+    });
+}
+
+function removeRows() {
+    const cells = document.querySelectorAll('.grid-item');
+    cells.forEach((cell) => {
+        cell.remove();
+    });
+}
+
+function makeRowsFromUserInput() {
+    const userInput = Number(prompt('Enter Grid Size:', lastGridSize))
+    if (userInput === 0 || userInput === lastGridSize) {
+        return;
+    } else if (isNaN(userInput) || !Number.isInteger(userInput)) {
+        alert('Please enter a whole number.');
+        return makeRowsFromUserInput();
+    } else {
+        removeRows();
+        makeRows(userInput, userInput);
+        addEventListenerToCells();
     }
 }
